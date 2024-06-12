c5dac36c85b59151bb1bce4defdebf0bd5e476f7
JuanAndresA016 <141958626+JuanAndresA016@users.noreply.github.com>
2024-06-11 22:30:53 -0600

    Missing data added

--- Series.cpp
+++ Series.cpp
@@ -3,8 +3,8 @@
 #include <numeric>
 using namespace std;
 
-Series::Series(int id, const string &name, int length, const string &genre)
-    : id(id), name(name), length(length), genre(genre) {}
+Series::Series(int id, const string &name, string name_of_espisodes, int amount, int length, const string &genre)
+    : id(id), name(name), name_of_episodes(name_of_episodes), amount(amount), length(length), genre(genre) {}
 
 void Series::addRating(int rating) {
     if (rating >= 1 && rating <= 5) {
