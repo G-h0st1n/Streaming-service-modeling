ceb16248420e450a82a8dac2eece33fd4717f7ab
Patricio <a01645013@tec.mx>
2024-06-11 20:34:56 -0600

    Added Video Superclass

---
+++ Video.h
@@ -0,0 +1,28 @@
+// Video.h
+#ifndef VIDEO_H
+#define VIDEO_H
+
+#include <string>
+#include <vector>
+
+using namespace std;
+
+class Video {
+protected:
+    int id;
+    string name;
+    int length; // in minutes
+    string genre;
+    vector<int> ratings;
+
+public:
+    Video(int id, const string &name, int length, const string &genre);
+    virtual ~Video() = default;
+
+    void addRating(int rating);
+    double getAverageRating() const;
+
+    virtual void display() const = 0; // Pure virtual function
+};
+
+#endif // VIDEO_H
